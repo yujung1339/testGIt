708847462326f87e7ab2a0295bca6d041c1106a1
yujung <148977734+yujung1339@users.noreply.github.com>
2024-03-30 18:32:16 +0900

    주석추가.

--- src/UpdateView.java
+++ src/UpdateView.java
@@ -82,7 +82,7 @@ public class UpdateView extends JFrame implements ActionListener {
             inputId.setText("");
             inputName.setText("");
             inputPw.setText("");
-            //..
+            //.......
 
         }
     }
